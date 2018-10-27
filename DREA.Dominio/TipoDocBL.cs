08f47cd96b5bde107dc3a07c4b99fe27659189c5
Richard Zapata <r_zapatac@hotmail.com>
2018-10-27 18:05:06 -0500

    docmentos

---
+++ DREA.Dominio/TipoDocBL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DREA.Modelo;
+
+namespace DREA.Dominio
+{
+   public class TipoDocBL: Repositorio<TipoDoc>
+    {
+
+    }
+}
---
+++ DREA/Controllers/DocumentoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DREA.Dominio;
+using DREA.Modelo;
+
+namespace DREA.Controllers
+{
+    public class DocumentoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(DocumentoBL.Listar());
+        }
+        public ActionResult Crear()
+        {
+            ViewBag.TipoDocId = new SelectList(TipoDocBL.Listar(), "TipoDocId", "Nombre");
+            ViewBag.OficinaId = new SelectList(OficinaBL.Listar(), "OficinaId", "Nombre");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Guardar(Documento doc)
+        {
+            doc.Fecha = DateTime.Now;
+            doc.Estado = "P";
+            doc.UsuarioId = 1;
+            DocumentoBL.Crear(doc);
+
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
